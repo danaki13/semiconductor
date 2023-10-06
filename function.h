b4a248f2248f177f0b6dd101f9a6d6d2da9d5867
Daniel Nagy (ThinkPad-X260 - Huntel) <dony.nagy@gmail.com>
2023-10-06 23:30:10 +0100

    Organised in class

--- function.h
+++ function.h
@@ -1,1 +1,16 @@
-double pn_diode_ideal(double);
+// MyClass.h
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+class pnDiodeIdeal {
+public:
+	/* data */
+	double Voltage;
+	double CurrentSaturartion;
+	double Temperature;
+
+    void set_values(double, double, double);
+	double pn_diode_ideal();
+}ObjPNDiodeIdeal;
+
+#endif
--- pn_diode.cpp
+++ pn_diode.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <cmath>
 
-double pn_diode_ideal(double Voltage) {
+class pnDiodeIdeal {
+public:
+	/* data */
+	double Voltage;
+	double CurrentSaturartion;
+	double Temperature;
+	
+	void set_values(double, double, double);
+	double pn_diode_ideal();
+	//~pnDiodeIdeal();
+};
+
+void pnDiodeIdeal::set_values(double voltV, double currentA, double tempK) {
+	Voltage = voltV;
+	CurrentSaturartion = currentA;
+	Temperature = tempK;
+}
+
+double pnDiodeIdeal::pn_diode_ideal() {
 	double CurrentDiode = 0.0;
-	double CurrentSaturartion = 1e-10, eCharge = 1.602e-19, kBoltzmann = 1.3806488e-23, Temperature = 300;
+	double eCharge = 1.602e-19, kBoltzmann = 1.3806488e-23;
 
 	CurrentDiode = CurrentSaturartion * (exp((eCharge * Voltage)/(kBoltzmann * Temperature)) - 1);
-	std::cout << "I_Diode = " << CurrentDiode << std::endl;
+	std::cout << Voltage << " " << CurrentDiode << std::endl;
 
 	return CurrentDiode;
 }
